b1f1880bf07cbd0afb50a034582adba006025886
ciglesia <ciglesia@student.42.fr>
2021-07-01 17:47:55 +0200

    libs-types

--- include/libstd.h
+++ include/libstd.h
@@ -6,12 +6,14 @@
 /*   By: ciglesia <ciglesia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 18:51:02 by ciglesia          #+#    #+#             */
-/*   Updated: 2021/06/30 23:37:19 by ciglesia         ###   ########.fr       */
+/*   Updated: 2021/07/01 17:01:59 by ciglesia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBSTD_H
 # define LIBSTD_H
+
+# include "libtypes.h"
 
 /*
 **	functions dependent on libstring:
@@ -25,70 +27,6 @@
 */
 
 # define BUFF_SIZE 6
-
-# include <sys/types.h>
-
-/*
-**	#define __SSIZE_T_TYPE		__SWORD_TYPE
-**	__STD_TYPE __SSIZE_T_TYPE __ssize_t;
-*/
-
-/*
-**	malloc
-*/
-
-# include <stdlib.h>
-
-/*
-**	Determine the wordsize from the preprocessor defines.
-*/
-
-# if defined __x86_64__ && !defined __ILP32__
-#  define __WORDSIZE	64
-# else
-#  define __WORDSIZE	32
-# endif
-
-/*
-**	Convenience types.
-*/
-
-typedef unsigned char							t_u_char;
-typedef unsigned short int						t_u_short;
-typedef unsigned int							t_u_int;
-typedef unsigned long int						t_u_long;
-
-# if __WORDSIZE == 64
-typedef signed long int							t_int64_t;
-typedef unsigned long int						t_uint64_t;
-# else
-__extension__ typedef signed long long int		t_int64_t;
-__extension__ typedef unsigned long long int	t_uint64_t;
-# endif
-
-/*
-**	quad_t is also 64 bits.
-*/
-
-# if __WORDSIZE == 64
-typedef long int								t_quad_t;
-typedef unsigned long int						t_u_quad_t;
-# else
-__extension__ typedef long long int				t_quad_t;
-__extension__ typedef unsigned long long int	t_u_quad_t;
-# endif
-
-/*
-**	Largest integral types.
-*/
-
-# if __WORDSIZE == 64
-typedef long int								t_intmax_t;
-typedef unsigned long int						t_uintmax_t;
-# else
-__extension__ typedef long long int				t_intmax_t;
-__extension__ typedef unsigned long long int	t_uintmax_t;
-# endif
 
 # define BOLD "\e[1m"
 # define DIM "\e[2m"
